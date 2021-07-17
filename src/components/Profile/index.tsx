0e3ce7bdf5ae3781e8ab9547946aaf8648eded27
Marcelo <mrclgst10@gmail.com>
2021-07-16 22:45:01 -0300

    add button to open SignOut component in modal

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import { useAuth } from '../../hooks/auth';
 import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
-  const { user } = useAuth();
+  const { user, toggleOpenModalToSignOut } = useAuth();
+
+  function handleOpenModalToSignOut() {
+    toggleOpenModalToSignOut();
+  }
 
   return (
     <View style={styles.container}>
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleOpenModalToSignOut}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá, </Text>
