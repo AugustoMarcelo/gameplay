70e4dd1c362be8a2993464ebf1aea995ca62f000
Marcelo <mrclgst10@gmail.com>
2021-07-16 22:46:18 -0300

    add container around guild icon

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { Image, Text } from 'react-native';
+import { Image, View } from 'react-native';
 
+import DiscordSvg from '../../assets/discord.svg';
 import { styles } from './styles';
 
-export function GuildIcon() {
-  const uri = 'https://assets.materialup.com/uploads/c4ea2753-8ad6-49b6-9d3e-68eb8564e803/teaser.png';
+const { CDN_IMAGE } = process.env;
 
+type GuildIconProps = {
+  guildId: string;
+  iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: GuildIconProps) {
+  // const uri = 'https://assets.materialup.com/uploads/c4ea2753-8ad6-49b6-9d3e-68eb8564e803/teaser.png';
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
   return (
-    <Image source={{ uri }} style={styles.image} resizeMode="cover" />
+    <View style={styles.container}>
+      {
+        iconId
+          ? <Image source={{ uri }} style={styles.image} resizeMode="cover" />
+          : <DiscordSvg width={40} height={40} />
+      }
+    </View>
   )
 }
