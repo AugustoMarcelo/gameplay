21c4cbc07b2c48884523937a681476fcb07280b2
Marcelo <mrclgst10@gmail.com>
2021-07-16 22:39:22 -0300

    fetch guilds from discord api

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { FlatList } from "react-native";
 import { View } from "react-native";
 import { Guild, GuildEntity } from "../../components/Guild";
 import { ListSeparator } from "../../components/ListSeparator";
+import { Loading } from "../../components/Loading";
+import { api } from "../../services/api";
 
 import { styles } from './styles'
 
@@ -11,39 +15,43 @@ type GuildsProps = {
 }
 
 export function Guilds({ handleSelectGuild }: GuildsProps) {
-  const guilds = [
-    {
-      id: '1',
-      name: 'Lendários',
-      icon: 'image.png',
-      owner: true
-    },
-    {
-      id: '2',
-      name: 'Lendários',
-      icon: 'image.png',
-      owner: true
-    },
-  ];
+  const [guilds, setGuilds] = useState<GuildEntity[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function fetchGuilds() {
+    const response = await api.get('/users/@me/guilds');
+
+    setGuilds(response.data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchGuilds();
+  }, [])
 
   return (
     <View style={styles.container}>
-      <FlatList
-        style={styles.guilds}
-        data={guilds}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <Guild
-            data={item}
-            onPress={() => handleSelectGuild(item)}
+      {isLoading
+        ? <Loading />
+        : (
+          <FlatList
+            style={styles.guilds}
+            data={guilds}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => (
+              <Guild
+                data={item}
+                onPress={() => handleSelectGuild(item)}
+              />
+            )}
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 68 }}
+            ItemSeparatorComponent={() => 
+              <ListSeparator style={styles.customListSeparator} />
+            }
           />
-        )}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{ paddingBottom: 68 }}
-        ItemSeparatorComponent={() => 
-          <ListSeparator style={styles.customListSeparator} />
-        }
-      />
+        )
+      }
     </View>
   )
 }
