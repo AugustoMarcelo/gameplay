a73c9a05b8fa1a4a8533fe28c17db3e98c38f699
Marcelo <mrclgst10@gmail.com>
2021-07-16 22:43:35 -0300

    add LinearGradient component

--- src/components/TextAreaInput/index.tsx
+++ src/components/TextAreaInput/index.tsx
@@ -1,13 +1,21 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { TextInput, TextInputProps } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 import { styles } from './styles';
 
 export function TextAreaInput({ ...rest }: TextInputProps) {
+  const { secondary50, secondary70 } = theme.colors;
   return (
-    <TextInput
+    <LinearGradient
+      colors={[secondary50, secondary70]}
       style={styles.container}
-      {...rest}
-    />
+    >
+      <TextInput
+        style={styles.textContainer}
+        {...rest}
+      />
+    </LinearGradient>
   )
 }
--- src/components/TextAreaInput/styles.ts
+++ src/components/TextAreaInput/styles.ts
@@ -5,16 +5,18 @@ export const styles = StyleSheet.create({
   container: {
     width: '100%',
     height: 95,
-    backgroundColor: theme.colors.secondary40,
-    color: theme.colors.heading,
     borderRadius: 8,
-    fontFamily: theme.fonts.text400,
-    fontSize: 13,
-    marginRight: 4,
     borderWidth: 1,
     borderColor: theme.colors.secondary50,
+  },
+  textContainer: {
+    width: '100%',
+    height: 95,
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
     paddingHorizontal: 16,
     paddingTop: 16,
     textAlignVertical: 'top'
-  },
+  }
 })
