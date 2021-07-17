8cb0bfb4cf536adbcf2e6716c329d9a8b3845f4e
Marcelo <mrclgst10@gmail.com>
2021-07-16 22:43:52 -0300

    add LinearGradient component

--- src/components/SmallInput/index.tsx
+++ src/components/SmallInput/index.tsx
@@ -1,14 +1,22 @@
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { TextInput, TextInputProps } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 import { styles } from './styles';
 
 export function SmallInput({ ...rest }: TextInputProps) {
+  const { secondary50, secondary70 } = theme.colors;
   return (
-    <TextInput
+    <LinearGradient
+      colors={[secondary50, secondary70]}
       style={styles.container}
-      keyboardType="numeric"
-      {...rest}
-    />
+    >
+      <TextInput
+        style={styles.textContainer}
+        keyboardType="numeric"
+        {...rest}
+      />
+    </LinearGradient>
   )
 }
--- src/components/SmallInput/styles.ts
+++ src/components/SmallInput/styles.ts
@@ -3,16 +3,21 @@ import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
-    width: 48,
+    width: 64,
     height: 48,
-    backgroundColor: theme.colors.secondary40,
-    color: theme.colors.heading,
     borderRadius: 8,
-    fontFamily: theme.fonts.text400,
-    fontSize: 13,
     marginRight: 4,
-    textAlign: 'center',
     borderWidth: 1,
     borderColor: theme.colors.secondary50,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
+  textContainer: {
+    width: 64,
+    height: 48,
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text500,
+    fontSize: 15,
+    textAlign: 'center',
+  }
 })
